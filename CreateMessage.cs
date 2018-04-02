24174b26fae0b30e0eee816ba36efe28ccdcb845
JoelMiller74 <joel.miller@pc-pipeline.com>
2018-04-01 21:46:17 -0500

    First commit

---
+++ CreateMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
