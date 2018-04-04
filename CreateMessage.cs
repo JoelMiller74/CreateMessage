bf9ace8dafe661450093e8afdf9aafe8fc8e5f09
Joel Miller <joel.miller@InfiniteCampus.com>
2018-04-03 19:59:47 -0500

    removed whitespace for readability

--- CreateMessage.cs
+++ CreateMessage.cs
@@ -13,27 +13,21 @@ class NewTask
           .Build();
         string RabbitMQHost = config["RabbitMQHost"];
         string RabbitMQQueueName = config["RabbitMQQueueName"];
-        
         var factory = new ConnectionFactory() { HostName = RabbitMQHost };
         using(var connection = factory.CreateConnection())
         using(var channel = connection.CreateModel())
         {
             channel.QueueDeclare(queue: RabbitMQQueueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
-
             var message = GetMessage(args);
             var body = Encoding.UTF8.GetBytes(message);
-
             var properties = channel.CreateBasicProperties();
             properties.Persistent = true;
-
             channel.BasicPublish(exchange: "", routingKey: RabbitMQQueueName, basicProperties: properties, body: body);
             Console.WriteLine(" [x] Sent {0}", message);
         }
-
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
     }
-
     private static string GetMessage(string[] args)
     {
         return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
